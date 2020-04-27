359d5468e36b581ff3ffdfac8e13f9462def18ae
OlegBabiak <ovbabiak@gmail.com>
2020-04-27 13:32:16 +0300

    Initial structure

---
+++ src/main/java/lviv/university/committee/AdmissionCommitteeProjectApplication.java
@@ -0,0 +1,13 @@
+package lviv.university.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionCommitteeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionCommitteeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lviv/university/committee/AdmissionCommitteeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lviv.university.committee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionCommitteeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
